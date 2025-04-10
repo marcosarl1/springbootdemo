db998f075685edfa01acc9258838a3347cf6df1a
Marcos Júnior <marcosajr11@gmail.com>
2025-04-10 18:15:40 -0300

    minor: Constructor without id

--- src/main/java/com/course/model/User.java
+++ src/main/java/com/course/model/User.java
@@ -18,8 +18,8 @@ public class User {
 
     public User() {
     }
-    public User(Long id, String name, String email, String phone, String password) {
-        this.id = id;
+
+    public User(String name, String email, String phone, String password) {
         this.name = name;
         this.email = email;
         this.phone = phone;
