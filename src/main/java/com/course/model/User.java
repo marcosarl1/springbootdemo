2de66cf528df0b55608af1de8a5fed904f01e27f
Marcos Júnior <marcosajr11@gmail.com>
2025-04-10 21:15:51 -0300

    feat: Order class

--- src/main/java/com/course/model/User.java
+++ src/main/java/com/course/model/User.java
@@ -2,6 +2,8 @@ package com.course.model;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -15,6 +17,9 @@ public class User {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -66,6 +71,9 @@ public class User {
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
 
     @Override
     public boolean equals(Object o) {
