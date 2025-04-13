872464f3aee53db3ba0f3aa14d7ad735934f4372
Marcos Júnior <marcosajr11@gmail.com>
2025-04-13 20:12:59 -0300

    Order repository

---
+++ src/main/java/com/course/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.course.repository;
+
+import com.course.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
